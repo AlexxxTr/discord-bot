144c34f83cf0dc5b6c65b3f48dc62d2c0430f9a9
Alexander Truyaert <alexander.truyaert@outlook.com>
2022-12-28 19:07:12 +0100

    AT: Registered the commands in a different way as they do it in the tutorial because this is now typescript based

--- src/index.ts
+++ src/index.ts
@@ -5,14 +5,41 @@ import { config } from "dotenv";
 
 config();
 
-// Create a new client instance
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
-// When the client is ready, run this code (only once)
-// We use 'c' for the event parameter to keep it separate from the already defined 'client'
+const commands = new Collection<string, any>();
+
+const commandsPath = path.join(__dirname, "commands");
+const commandFiles = fs
+  .readdirSync(commandsPath)
+  .filter((file) => file.endsWith(".ts"));
+
+for (const file of commandFiles) {
+  const filePath = path.join(commandsPath, file);
+  const command = require(filePath);
+  commands.set(command.data.name, command);
+}
+
 client.once(Events.ClientReady, (c) => {
   console.log(`Ready! Logged in as ${c.user.tag}`);
 });
 
-// Log in to Discord with your client's token
+client.on(Events.InteractionCreate, async (interaction) => {
+  if (!interaction.isChatInputCommand()) return;
+
+  const command = commands.get(interaction.commandName);
+
+  if (!command) return;
+
+  try {
+    await command.execute(interaction);
+  } catch (error) {
+    console.error(error);
+    await interaction.reply({
+      content: "There was an error while executing this command!",
+      ephemeral: true,
+    });
+  }
+});
+
 client.login(process.env.TOKEN);
